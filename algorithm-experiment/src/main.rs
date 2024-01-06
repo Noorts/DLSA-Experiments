f7bcec5b68b3a82f736aaf14f3b9c62a85cc2016
Daniel Voogsgerd <daniel@voogsgerd.nl>
2024-01-06 12:42:55 +0100

    Add algorithm benchmarks
    Now we have proper benchmarks of the algorithm

---
+++ algorithm-experiment/src/main.rs
@@ -0,0 +1,162 @@
+use std::{
+    fs::File,
+    io::Write,
+    path::{Path, PathBuf},
+    time::Instant,
+};
+
+use rand::{distributions::Uniform, Rng};
+use serde::Serialize;
+use sw::{
+    algorithm::find_alignment_simd_lowmem, algorithm::AlignmentScores,
+    find_alignment_sequential_straight, find_alignment_simd,
+};
+
+#[derive(Serialize)]
+struct Measurements {
+    name: String,
+    query_size: usize,
+    target_size: usize,
+    iterations: usize,
+    times: Vec<usize>,
+}
+
+const WARMUP: usize = 10;
+const ITER_PER_SAMPLE: usize = 10;
+const SAMPLES: usize = 100;
+const QUERY_SIZE: usize = 64 * 5;
+const TARGET_SIZE: usize = 1024 * 32;
+
+type AlignResult = (Vec<char>, Vec<char>, i16, usize, usize);
+type AlignFn = fn(&[char], &[char], AlignmentScores) -> AlignResult;
+
+const NEUCLEOTIDES: [char; 4] = ['A', 'C', 'G', 'T'];
+
+fn main() {
+    let query: Vec<char> = std::iter::repeat('A').take(QUERY_SIZE).collect();
+
+    let target_disjoint: Vec<char> = std::iter::repeat('G').take(TARGET_SIZE).collect();
+    let target_equal: Vec<char> = std::iter::repeat('A').take(TARGET_SIZE).collect();
+
+    let target_random: Vec<char> = rand::thread_rng()
+        .sample_iter(Uniform::from(1..4))
+        .map(|x| NEUCLEOTIDES[x])
+        .take(TARGET_SIZE)
+        .collect();
+
+    let targets = [
+        ("equal", target_equal.clone()),
+        ("disjoint", target_disjoint.clone()),
+        ("random", target_random.clone()),
+    ];
+
+    let scores = AlignmentScores {
+        gap: -2,
+        r#match: 3,
+        miss: -3,
+    };
+
+    let algs: Vec<(String, AlignFn)> = vec![
+        (String::from("01"), find_alignment_simd_lowmem::<01>),
+        (String::from("02"), find_alignment_simd_lowmem::<02>),
+        (String::from("04"), find_alignment_simd_lowmem::<04>),
+        (String::from("08"), find_alignment_simd_lowmem::<08>),
+        (String::from("16"), find_alignment_simd_lowmem::<16>),
+        (String::from("32"), find_alignment_simd_lowmem::<32>),
+        (String::from("64"), find_alignment_simd_lowmem::<64>),
+    ];
+
+    for (target_name, target) in &targets {
+        for (name, alg) in &algs {
+            let measurement = benchmark(name, *alg, &query, &target, scores);
+            let path = PathBuf::from(format!(
+                "results/simd_lanes_ringbuf/{}/{}.json",
+                target_name, name
+            ));
+
+            ensure_directory(&path);
+
+            let mut file = File::create(&path).expect("Could not open result file");
+            let data =
+                serde_json::to_string(&measurement).expect("Could not convert measurement to json");
+            file.write(data.as_bytes())
+                .expect("Could not write results");
+        }
+    }
+
+    let algs: Vec<(String, AlignFn)> = vec![
+        (String::from("01"), find_alignment_simd::<01>),
+        (String::from("02"), find_alignment_simd::<02>),
+        (String::from("04"), find_alignment_simd::<04>),
+        (String::from("08"), find_alignment_simd::<08>),
+        (String::from("16"), find_alignment_simd::<16>),
+        (String::from("32"), find_alignment_simd::<32>),
+        (String::from("64"), find_alignment_simd::<64>),
+    ];
+
+    for (target_name, target) in &targets {
+        for (name, alg) in &algs {
+            let measurement = benchmark(name, *alg, &query, &target, scores);
+            let path = PathBuf::from(format!("results/simd_lanes/{target_name}/{}.json", name));
+            ensure_directory(&path);
+            let mut file = File::create(&path).expect("Could not open result file");
+            let data =
+                serde_json::to_string(&measurement).expect("Could not convert measurement to json");
+            file.write(data.as_bytes())
+                .expect("Could not write results");
+        }
+    }
+
+    // Sequential
+    let measurement = benchmark(
+        "Sequential",
+        find_alignment_sequential_straight,
+        &query,
+        &target_equal,
+        scores,
+    );
+    let path = PathBuf::from("results/sequential.json");
+    ensure_directory(&path);
+    let mut file = File::create(&path).expect("Could not open result file");
+    let data = serde_json::to_string(&measurement).expect("Could not convert measurement to json");
+    file.write(data.as_bytes())
+        .expect("Could not write results");
+}
+
+fn ensure_directory<P: AsRef<Path>>(path: P) {
+    if let Some(parent) = path.as_ref().parent() {
+        std::fs::create_dir_all(parent).expect("Could not create directory");
+    }
+}
+
+fn benchmark(
+    name: &str,
+    alignment_function: fn(&[char], &[char], AlignmentScores) -> AlignResult,
+    query: &[char],
+    target: &[char],
+    scores: AlignmentScores,
+) -> Measurements {
+    let mut times = Vec::new();
+    for _ in 0..WARMUP {
+        let _ = alignment_function(&query, &target, scores);
+    }
+
+    for _ in 0..SAMPLES {
+        let start = Instant::now();
+        for _ in 0..ITER_PER_SAMPLE {
+            let _ = alignment_function(&query, &target, scores);
+        }
+        let end = Instant::now();
+        let duration = (end - start) / ITER_PER_SAMPLE as u32;
+
+        times.push(duration.as_nanos() as usize);
+    }
+
+    Measurements {
+        name: name.to_string(),
+        query_size: query.len(),
+        target_size: target.len(),
+        iterations: ITER_PER_SAMPLE,
+        times,
+    }
+}
---
+++ plotting/algorithm/simd_lanes.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+import matplotlib.pyplot as plt
+import json
+import numpy as np
+import scipy as sp
+from scipy import stats
+import sys
+import os
+
+plt.style.use([
+    os.path.join(os.path.dirname(__file__), "../resources/vu.mplstyle"),
+    os.path.join(os.path.dirname(__file__), "../resources/twocolumn.mplstyle"),
+])
+
+RESULT_DIR = os.path.join(os.path.dirname(__file__), "../../algorithm-experiment/results/")
+
+def amdahl(s, p):
+    return 1 / ((1-p) + (p/s))
+
+def main():
+    plot_lane_scaling("simd_lanes", "equal")
+    plot_lane_scaling("simd_lanes_ringbuf", "equal")
+    plot_lane_scaling_input("simd_lanes_ringbuf")
+
+    plt.show()
+
+def plot_lane_scaling_input(directory):
+    sequential_result = read_file(os.path.join(RESULT_DIR, "sequential.json"))
+    sequential_cups = get_cups(sequential_result) / 1e6
+    fig = plt.figure()
+
+    # Figure
+    ax = fig.add_subplot(111)
+    ax.set_xlabel("Lane count")
+    ax.set_ylabel("MCUPS")
+
+    for target_name in ["disjoint", "random", "equal"]:
+        files = [
+            (1, f"{directory}/{target_name}/01.json"),
+            (2, f"{directory}/{target_name}/02.json"),
+            (4, f"{directory}/{target_name}/04.json"),
+            (8, f"{directory}/{target_name}/08.json"),
+            (16, f"{directory}/{target_name}/16.json"),
+            (32, f"{directory}/{target_name}/32.json"),
+            (64, f"{directory}/{target_name}/64.json"),
+        ]
+
+        measurements = [(name, read_file(os.path.join(RESULT_DIR, file))) for (name, file) in files]
+
+        lane_counts = np.array([name for name, _ in measurements])
+        mcups = np.array([get_cups(measurement) / 1e6 for _, measurement in measurements]).T
+
+        print(len(lane_counts), len(mcups))
+        median = np.median(mcups, axis=0)
+        upper_bound = np.max(mcups, axis=0) - median
+        lower_bound = median - np.min(mcups, axis=0)
+        yerr = np.vstack((lower_bound, upper_bound))
+        ax.errorbar(lane_counts, median, yerr=yerr, fmt=".", capsize=3, label=target_name)
+        # ax.errorbar(lane_counts, median, yerr=yerr, fmt=None, label=target_name)
+
+    ax.axhline(np.median(sequential_cups), label="Sequential", ls="--", color="black")
+    ax.set_xticks(lane_counts)
+
+    ax.legend()
+    fig.tight_layout()
+
+def plot_lane_scaling(directory, target_name):
+    sequential_result = read_file(os.path.join(RESULT_DIR, "sequential.json"))
+    sequential_cups = get_cups(sequential_result) / 1e6
+
+    files = [
+        (1, f"{directory}/{target_name}/01.json"),
+        (2, f"{directory}/{target_name}/02.json"),
+        (4, f"{directory}/{target_name}/04.json"),
+        (8, f"{directory}/{target_name}/08.json"),
+        (16, f"{directory}/{target_name}/16.json"),
+        (32, f"{directory}/{target_name}/32.json"),
+        (64, f"{directory}/{target_name}/64.json"),
+    ]
+
+    measurements = [(name, read_file(os.path.join(RESULT_DIR, file))) for (name, file) in files]
+
+    lane_counts = np.array([name for name, _ in measurements])
+    mcups = np.array([get_cups(measurement) / 1e6 for _, measurement in measurements]).T
+
+    time_normalize_var = np.median(mcups[:, 0])
+    mcups = mcups / time_normalize_var
+
+    # Figure
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.boxplot(mcups, positions=lane_counts, notch=True, widths=3)
+    ax.set_xlabel("Lane count")
+    ax.set_ylabel("Speedup")
+
+    # Amdahl's law
+    popt, pcov = sp.optimize.curve_fit(amdahl, lane_counts[:-2], np.median(mcups, axis=0)[:-2])
+    cont_lane_count = np.linspace(lane_counts[0], lane_counts[-3], 100000)
+    ax.plot(cont_lane_count, amdahl(cont_lane_count, *popt), ls="--", label=f"Amdahls law fit: p={popt[0]:.2f}")
+    print("Condition number", np.linalg.cond(pcov))
+    print("RMS: ", np.sqrt(np.mean(np.power(amdahl(lane_counts[:-2], *popt) - np.median(mcups, axis=0)[:-2], 2))))
+
+
+    # Amdahl's law
+    popt, pcov = sp.optimize.curve_fit(amdahl, lane_counts, np.median(mcups, axis=0))
+    cont_lane_count = np.linspace(lane_counts[0], lane_counts[-1], 100000)
+    ax.plot(cont_lane_count, amdahl(cont_lane_count, *popt), ls="--", label=f"Amdahls law fit: p={popt[0]:.2f}")
+    print("Condition number", np.linalg.cond(pcov))
+    print("RMS: ", np.sqrt(np.mean(np.power(amdahl(lane_counts, *popt) - np.median(mcups, axis=0), 2))))
+
+    # Linear axis
+    lim = ax.get_ylim()
+    ax.plot([0, lane_counts[-1]], [0, lane_counts[-1]], ls="--", label="Linear scaling")
+    ax.set_ylim(lim)
+    ax.axhline(np.median(sequential_cups)/time_normalize_var, label="Sequential", ls="--", color="black")
+
+
+    # Second CUPS y-axis
+    ax2 = ax.twinx()
+    ax_min, ax_max = ax.get_ylim()
+    ax2.set_ylim(ax_min * time_normalize_var, ax_max * time_normalize_var)
+    ax2.set_ylabel("MCUPS")
+
+    ax.legend()
+    fig.tight_layout()
+
+def plot_histogram(data):
+    plt.hist(data["times"], bins = 100)
+    res = stats.bootstrap((data["times"],), np.median, confidence_level=0.95)
+    print(res.confidence_interval)
+    mean = np.mean(data["times"])
+    low = res.confidence_interval.low
+    high = res.confidence_interval.high
+    print(f"Mean: {mean}; Low: {low}; High: {high}")
+
+    plt.axvline(low, color="red")
+    plt.axvline(mean, color="purple")
+    plt.axvline(high, color="red")
+
+def get_cups(data):
+    return data["query_size"] * data["target_size"] / np.array(data["times"]) * 1e9
+
+def read_file(filename):
+    with open(filename, "r") as f:
+        data = json.load(f)
+        return data
+
+
+if __name__ == '__main__':
+    main()
